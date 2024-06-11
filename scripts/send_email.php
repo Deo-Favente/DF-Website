a1abc5d3ff8c5b404f9bb21e27a173a92230ec32
Deo_Favente <56390262+Deo-Favente@users.noreply.github.com>
2024-06-12 01:49:18 +0200

    Dernier fixs

--- scripts/send_email.php
+++ scripts/send_email.php
@@ -1,9 +1,6 @@
 <?php
 $errors = [];
 
-//ini_set('smtp_port', 587);
-//ini_set('SMTP', 'smtp.gmail.com');
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get POST data
     $name = isset($_POST['name']) ? strip_tags(trim($_POST['name'])) : '';
@@ -12,46 +9,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Validate form fields
     if (empty($name)) {
-        $errors[] = 'Name is empty';
+        $errors[] = 'Veuillez renseigner un nom';
     }
 
     if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
+        $errors[] = 'Veuillez renseigner un email';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && $email != "???") {
+        $errors[] = 'Veuillez renseigner un email valide';
     }
 
     if (empty($message)) {
-        $errors[] = 'Message is empty';
+        $errors[] = 'Veuillez renseigner un message';
     }
 
     // If no errors, send email
     if (empty($errors)) {
+
+        if ($name == "???" && $email == "???" && $message == "???") {
+            header('Location: https://deo-favente.fr/sorsdematete');
+            exit;
+        }
+
         // Recipient email address (replace with your own)
         $recipient = "deofavente.dev@gmail.com";
 
         // Subject
-        $subject = "New message from contact form";
+        $subject = "Nouveau message de $name";
 
         // Email headers
-        $headers = "From: $name <$email>\r\n";
-        $headers .= "Reply-To: $email\r\n";
+        $headers = "Reply-To: $email\r\n";
         $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
         // Email body
-        $email_body = "Name: $name\n";
+        $email_body = "Nom: $name\n";
         $email_body .= "Email: $email\n\n";
         $email_body .= "Message:\n$message\n";
 
         // Send email
         if (mail($recipient, $subject, $email_body, $headers)) {
-            echo "Email sent successfully!";
+            // Email sent, redirect to home page
+            header("Location: /");
         } else {
-            echo "Failed to send email. Please try again later.";
+            echo "Votre message n'a pas pu être envoyé, veuillez réessayer plus tard.";
         }
     } else {
         // Display errors
-        echo "The form contains the following errors:<br>";
+        echo "Les erreurs suivantes se sont produites:<br>";
         foreach ($errors as $error) {
             echo "- $error<br>";
         }
@@ -59,6 +62,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     // Not a POST request, display a 403 forbidden error
     header("HTTP/1.1 403 Forbidden");
-    echo "You are not allowed to access this page.";
+    echo "403 Forbidden";
 }
 ?>
